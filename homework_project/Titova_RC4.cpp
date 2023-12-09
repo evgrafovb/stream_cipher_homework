1c527e9175fdab42d9b4c60a3432e4c8551bee28
Anastashiine <anastashiine@yandex.ru>
2023-12-09 18:04:36 +0300

    add function to main|add text and key word

--- homework_project/Titova_RC4.cpp
+++ homework_project/Titova_RC4.cpp
@@ -41,5 +41,31 @@ vector<unsigned char> RC4_Decipher(vector<unsigned char> cipher_text, vector<uns
 
 int main
 {
+    vector<unsigned char> text = { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!' };
+    vector<unsigned char> key = { 'c', 'i', 'p', 'h', 'e', 'r' };
+    vector<unsigned char> cipher_text = RC4_Cipher(text, key);
+    vector<unsigned char> decipher_text = RC4_Decipher(cipher_text, key);
+
+    cout << "Text: ";
+    for (unsigned char byte: text)
+    {
+        cout << byte;
+    }
+    cout << endl;
+
+    cout << "Cipher_text: ";
+    for (unsigned char byte: cipher_text)
+    {
+        cout << byte;
+    }
+    cout << endl;
+
+    cout << "Decipher_text: ";
+    for (unsigned char byte: decipher_text)
+    {
+        cout << byte;
+    }
+    cout << endl;
+
 	return 0;
 }
