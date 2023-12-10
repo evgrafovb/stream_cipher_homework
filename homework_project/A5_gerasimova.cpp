91c2ea49e46fb19f7cfd61b0e7a94ecea9354c78
unicornaa <anastasia.gerasimova.03@mail.ru>
2023-12-10 13:46:27 +0300

    main, key, cin

---
+++ homework_project/A5_gerasimova.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	unsigned long long key = 0x1234567890ABCDEF; // 64-bit key
+	unsigned long long frame = 0x134; // Frame number
+
+	cout << "Enter a Text: ";
+	string plaintext;
+	getline(cin, plaintext);
+}
