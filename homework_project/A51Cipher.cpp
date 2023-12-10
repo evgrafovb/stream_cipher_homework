5644ac7018d8aa033ee94ef9e3f88971e6df3568
unicornaa <anastasia.gerasimova.03@mail.ru>
2023-12-10 15:41:50 +0300

    get

--- homework_project/A51Cipher.cpp
+++ homework_project/A51Cipher.cpp
@@ -18,3 +18,46 @@ A51Cipher::A51Cipher(unsigned long long key, unsigned long long frame)
         clockWithMajority();
     }
 }
+
+bool A51Cipher::majorityBit() const
+{
+    return (R1[8] + R2[10] + R3[10]) >= 2;
+}
+
+void A51Cipher::clockWithMajority() {
+    bool majority = majorityBit();
+    if (R1[8] == majority) {
+        bool new_bit = R1[13] ^ R1[16] ^ R1[17] ^ R1[18];
+        R1 >>= 1;
+        R1[18] = new_bit;
+    }
+    if (R2[10] == majority) {
+        bool new_bit = R2[20] ^ R2[21];
+        R2 >>= 1;
+        R2[21] = new_bit;
+    }
+    if (R3[10] == majority) {
+        bool new_bit = R3[7] ^ R3[20] ^ R3[21] ^ R3[22];
+        R3 >>= 1;
+        R3[22] = new_bit;
+    }
+}
+
+char A51Cipher::getKeystreamByte() {
+    char keystream = 0;
+    for (int i = 0; i < 8; ++i) {
+        clockWithMajority();
+        bool bit = R1[18] ^ R2[21] ^ R3[22];
+        keystream |= (bit << i);
+    }
+    return keystream;
+}
+
+string A51Cipher::encrypt(const string& plaintext) {
+    string ciphertext;
+    for (char c : plaintext) {
+        char keystream = getKeystreamByte();
+        ciphertext += c ^ keystream;
+    }
+    return ciphertext;
+}
--- homework_project/A51Cipher.h
+++ homework_project/A51Cipher.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef A51CIPHER_H
+#define A51CIPHER_H
 
 #include <bitset>
 #include <string>
@@ -12,7 +14,13 @@ private:
     bitset<22> R2;
     bitset<23> R3;
 
+    bool majorityBit() const;
+    void clockWithMajority();
+
 public:
     A51Cipher(unsigned long long key, unsigned long long frame);
+    char getKeystreamByte();
+    string encrypt(const string& plaintext);
 };
 
+#endif // A51CIPHER_H
